48fcf5326fdc6e8ea81172faf37399c2c282617b
Aniket Jain <63732758+AniketJain17@users.noreply.github.com>
2022-01-23 20:43:35 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "2. What symbol indicates a tag?",
+    answer: "Angle brackets e.g.<>",
+    options: [
+      "Angle brackets e.g.<>",
+      "Curved brackets e.g. {,}",
+      "Commas e.g. ','",
+      "Exclamation marks e.g. !"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+   {
+    numb: 6,
+    question: "Whats so great about XML?",
+    answer: "Both A & B",
+    options: [
+      "Easy data exchange",
+      "High speed on network",
+      "Only B.is correct",
+      "Both A & B"
+    ]
+  },
+  {
+    numb: 7,
+    question: " Which is not a property of attribute Behaviour of 'Marquee' tag?",
+    answer: "blur",
+    options: [
+      "alternate",
+      "blur",
+      "scroll",
+      "slide"
+    ]
+  },
+  {
+    numb: 8,
+    question: "The attribute used to define a new namespace is.?",
+    answer: "Xmlns",
+    options: [
+      "XMLNS",
+      "XmlNameSpace",
+      "Xmlns",
+      "XmlNs"
+    ]
+  },
+  {
+    numb: 9,
+    question: " _________ keyword is used to declare variables in javascript?",
+    answer: "Var",
+    options: [
+      "Var",
+      "Dim",
+      "String",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 10,
+    question: " Which of the following JavaScript cannot do??",
+    answer: "All of the Above",
+    options: [
+      "JavaScript can react to events",
+      "JavaScript can manipulate HTML elements",
+      "JavaScript can be use to validate data",
+      "All of the Above"
+    ]
+  },
+];
